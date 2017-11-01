e9d7904197739c9e09a552f14fa33559fc676b6b
HerbertBodner <herbert.bodner@gmail.com>
2017-11-01 20:06:31 +0100

    Appveyor integration and docker build script

---
+++ Build.ps1
@@ -0,0 +1,30 @@
+function Exec
+{
+    [CmdletBinding()]
+    param(
+        [Parameter(Position=0,Mandatory=1)][scriptblock]$cmd,
+        [Parameter(Position=1,Mandatory=0)][string]$errorMessage = ($msgs.error_bad_command -f $cmd)
+    )
+    echo "Exec: $cmd"
+    & $cmd
+    if ($lastexitcode -ne 0) {
+        throw ("Exec: " + $errorMessage)
+    }
+}
+
+$solution_name="WaCore.TalentHunter.sln"
+
+echo "Restore packages"
+exec { & dotnet restore .\src\$solution_name }
+
+#to delete
+#exec { & dotnet build -c Release .\src }
+
+Push-Location .\src
+    $publish_path = "./bin/Release/netcoreapp2.0/publish/"
+    echo "Publish to: $publish_path"
+    dotnet publish -c release .\$solution_name -o $publish_path
+
+    # specify build path from dockerfile point-of-view (not from docker-compose.yml point-of-view)
+    exec { & docker-compose build --build-arg "source=$publish_path" wacore.talenthunter.api }
+Pop-Location
